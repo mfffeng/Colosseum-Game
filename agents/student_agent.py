20dd8168e38b529e790b9e6015331ab04dace67d
Minzhe Feng <75942143+mfffeng@users.noreply.github.com>
2022-04-04 00:27:49 -0400

    Feature complete

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -22,44 +22,6 @@ class StudentAgent(Agent):
         }
 
     @staticmethod
-    def check_status(board, my_pos, adv_pos):
-        father = dict()
-        for r in range(board.shape[0]):
-            for c in range(board.shape[1]):
-                father[(r, c)] = (r, c)
-
-        def find(pos):
-            if father[pos] != pos:
-                father[pos] = find(father[pos])
-            return father[pos]
-
-        def union(pos1, pos2):
-            father[pos1] = pos2
-
-        for r in range(board.shape[0]):
-            for c in range(board.shape[1]):
-                for dir, move in enumerate(
-                    ((0, 1), (1, 0))
-                ):  # Only check down and right
-                    if board[r, c, dir + 1]:
-                        continue
-                    pos_a = find((r, c))
-                    pos_b = find((r + move[0], c + move[1]))
-                    if pos_a != pos_b:
-                        union(pos_a, pos_b)
-
-        for r in range(board.shape[0]):
-            for c in range(board.shape[1]):
-                find((r, c))
-        p0_r = find(tuple(my_pos))
-        p1_r = find(tuple(adv_pos))
-        p0_score = list(father.values()).count(p0_r)
-        p1_score = list(father.values()).count(p1_r)
-        if p0_r == p1_r:
-            return False, -100000           # The game has not ended yet
-        return True, p0_score - p1_score    # The game has ended
-
-    @staticmethod
     def valid_steps(board, my_pos, adv_position, max_step, visited):
         visited.add(my_pos)
         if max_step == 0:
@@ -71,22 +33,23 @@ class StudentAgent(Agent):
             if not (0 <= x and x < board.shape[0] and 0 <= y and y < board.shape[1]):
                 continue
             # Can't go through boundaries
-            if num == 0 and board[x][y][2] == 1:
+            if num == 0 and board[my_pos[0], my_pos[1], 0]:
                 continue
-            if num == 1 and board[x][y][3] == 1:
+            if num == 1 and board[my_pos[0], my_pos[1], 1]:
                 continue
-            if num == 2 and board[x][y][0] == 1:
+            if num == 2 and board[my_pos[0], my_pos[1], 2]:
                 continue
-            if num == 3 and board[x][y][1] == 1:
+            if num == 3 and board[my_pos[0], my_pos[1], 3]:
                 continue
+            can_explore = False
             for direction in (0, 1, 2, 3):
-                if board[x][y][direction] == 0 and (x, y) != adv_position and (x, y) not in visited:
+                if not board[x, y, direction] and (x, y) != adv_position and (x, y) not in visited:
+                        can_explore = True
                         result.append((x, y, direction))
-            result += StudentAgent.valid_steps(board, (x, y), adv_position, max_step - 1, visited)
-        
+            if can_explore:
+                result += StudentAgent.valid_steps(board, (x, y), adv_position, max_step - 1, visited)
         # Experimental: sort the result to let moves that shorten the distance to the opponent be examined first
-        result = sorted(result, key=lambda x: (x[0] - adv_position[0]) ** 2 + (x[1] - adv_position[1] ** 2))
-        
+        result = sorted(result, key=lambda x: (x[0] - adv_position[0]) ** 2 + (x[1] - adv_position[1] ** 2))[:5]
         return result
 
     @staticmethod
@@ -95,12 +58,12 @@ class StudentAgent(Agent):
         for i in range(max(my_pos[0] - 2, 0), min(my_pos[0] + 2, board.shape[0])):
             for j in range(max(my_pos[1] - 2, 0), min(my_pos[1] + 2, board.shape[1])):
                 for k in range(4):
-                    if board[i][j][k]:
+                    if board[i, j, k]:
                         my_count -= 1
         for i in range(max(adv_pos[0] - 2, 0), min(adv_pos[0] + 2, board.shape[0])):
             for j in range(max(adv_pos[1] - 2, 0), min(adv_pos[1] + 2, board.shape[1])):
                 for k in range(4):
-                    if board[i][j][k]:
+                    if board[i, j, k]:
                         my_count += 1
         return my_count
 
@@ -111,27 +74,28 @@ class StudentAgent(Agent):
             if is_max:
                 return (-1, -1, -1, result)
             return (-1, -1, -1, -result)
-        
+        # Remove the end game check, since its evaluation metrics (based on the number of tiles each player control)
+        # is different from that of the early evaluation's, which is based on the nunmber of walls around each player..
+        ''''
         stats = StudentAgent.check_status(board, my_pos, adv_pos)
         if stats[0]:
             if is_max:
                 return (-1, -1, -1, stats[1])
-            return (-1, -1, -1, -stats[1])      # Invert the score if ending in min player's term
+            return (-1, -1, -1, -stats[1])      # Invert the score if ending in min player's term'''
         potential_steps = StudentAgent.valid_steps(board, my_pos, adv_pos, max_step, set())
-
-        '''
-        # It seems that stay where you are is likely to cause suicide
         for d in (0, 1, 2, 3):      # Stay where you are
-            if board[my_pos[0]][my_pos[1]][d] == 0:
-                potential_steps.append((my_pos[0], my_pos[1], d))'''
+            if board[my_pos[0], my_pos[1], d] == 0:
+                potential_steps.append((my_pos[0], my_pos[1], d))
         if is_max:
             best = (-1, -1, -1, -10000)
             for i in potential_steps:
-                board[i[0]][i[1]][i[2]] = True
+                board[i[0], i[1], i[2]] = True
                 val = StudentAgent.a_b_pruning(board, adv_pos, (i[0], i[1]), max_step, a, b, False, depth - 1)
-                board[i[0]][i[1]][i[2]] = False
-                val = (i[0], i[1], i[2], val[2])            
+                board[i[0], i[1], i[2]] = False
+                val = (i[0], i[1], i[2], val[3])            
                 best = best if best[3] > val[3] else val
+                if val[3] < best[3] and best[3] == -10000:
+                    print(f'max{val}')
                 a = a if a > best[3] else best[3]
                 if a >= b:
                     break
@@ -139,10 +103,10 @@ class StudentAgent(Agent):
         else:
             best = (-1, -1, -1, 10000)
             for i in potential_steps:
-                board[i[0]][i[1]][i[2]] = True
+                board[i[0], i[1], i[2]] = True
                 val = StudentAgent.a_b_pruning(board, adv_pos, (i[0], i[1]), max_step, a, b, True, depth - 1)
-                board[i[0]][i[1]][i[2]] = False            
-                val = (i[0], i[1], i[2], val[2])
+                board[i[0], i[1], i[2]] = False            
+                val = (i[0], i[1], i[2], val[3])
                 best = best if best[3] < val[3] else val
                 b = b if b < best[3] else best[3]
                 if a >= b:
@@ -164,6 +128,5 @@ class StudentAgent(Agent):
 
         Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
         """
-        # dummy return
         output = StudentAgent.a_b_pruning(chess_board, my_pos, adv_pos, max_step, -10000, 10000, True, 3)
         return output[:2], output[2]
